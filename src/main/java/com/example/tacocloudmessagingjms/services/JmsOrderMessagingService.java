c77cc280c0d47b106e1beeff8ced1433f4a98495
MrMatiz2 <alejandromatiz87@gmail.com>
2024-11-24 21:45:01 -0500

    Main taco-cloud dependency added

--- src/main/java/com/example/tacocloudmessagingjms/services/JmsOrderMessagingService.java
+++ src/main/java/com/example/tacocloudmessagingjms/services/JmsOrderMessagingService.java
@@ -1,6 +1,6 @@
 package com.example.tacocloudmessagingjms.services;
 
-import com.example.tacocloudmessagingjms.entities.TacoOrder;
+import com.example.tacocloud.entities.TacoOrder;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import jakarta.jms.Session;
--- src/main/java/com/example/tacocloudmessagingjms/services/OrderMessagingService.java
+++ src/main/java/com/example/tacocloudmessagingjms/services/OrderMessagingService.java
@@ -1,6 +1,6 @@
 package com.example.tacocloudmessagingjms.services;
 
-import com.example.tacocloudmessagingjms.entities.TacoOrder;
+import com.example.tacocloud.entities.TacoOrder;
 
 public interface OrderMessagingService {
 
